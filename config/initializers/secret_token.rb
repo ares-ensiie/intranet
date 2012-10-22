fe5b4ff765ccd0032c5459613c853d5e8d443c76
Soulou <leo@unbekandt.eu>
2012-10-22 18:02:36 +0200

    Base projet Ruby on Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IntranetSXB::Application.config.secret_token = 'aca7cd60ce1aa302c5e0ae3e005b36d7cccd63664e897a9a17e6bfa10fa46dd9175dc862c9514fce6edefb4255bc8936f94a403eea612a726e9db09aa5d83751'
