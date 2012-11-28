53785b727bdeb0a51d637ec057e30c441aea7caf
Soulou <leo@unbekandt.eu>
2012-11-28 23:22:39 +0100

    Fichiers de déploiement

--- Gemfile
+++ Gemfile
@@ -50,7 +50,7 @@ gem "rmagick"
 # gem 'unicorn'
 
 # Deploy with Capistrano
-# gem 'capistrano'
+gem 'capistrano'
 
 # To use debugger
 # gem 'ruby-debug19', :require => 'ruby-debug'
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,5 @@
+require 'bundler/capistrano'
+
 set :application, "ensiie-intranet"
 set :repository,  "ssh://git@ares-ensiie.eu:2222/ensiie_intranet.git"
 set :scm, :git
@@ -7,12 +9,18 @@ set :branch, "master"
 set :use_sudo, false
 set :deploy_to, "/home/intranet/"
 
+set :default_environment, {
+    'PATH' => "/home/intranet/.rbenv/shims:$PATH"
+}
+
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "ares-ensiie.eu:2201"                          # Your HTTP server, Apache/etc
-role :app, "ares-ensiie.eu:2201"                          # This may be the same as your `Web` server
-role :db,  "ares-ensiie.eu:2201", :primary => true # This is where Rails migrations will run
+server "ares-ensiie.eu:2201", :app, :web, :db, :primary => true
+
+# role :web, "ares-ensiie.eu:2201"                          # Your HTTP server, Apache/etc
+# role :app, "ares-ensiie.eu:2201"                          # This may be the same as your `Web` server
+# role :db,  "ares-ensiie.eu:2201", :primary => true # This is where Rails migrations will run
 # role :db,  "your slave db-server here"
 
 # if you want to clean up old releases on each deploy uncomment this:
