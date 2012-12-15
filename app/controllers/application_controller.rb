366a78ec6f1d78d92dc2b3d3eca3242a7d8f7a74
Paul Chobert <paul@chobert.fr>
2012-12-15 22:29:03 +0100

    protect staging env

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   respond_to :html, :json
+  prepend_before_filter :protect_application! if Rails.env.staging?
+
+  protected
+  def protect_application!
+    return true unless ENV['PROTECTION_PASSWORD']
+    authenticate_or_request_with_http_basic do |username, password|
+      password == ENV['PROTECTION_PASSWORD']
+    end
+  end
 end
