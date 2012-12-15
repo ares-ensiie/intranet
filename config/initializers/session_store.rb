5d528f15239166aa2092180ee9d5068968cdfa0e
Paul Chobert <paul@chobert.fr>
2012-12-15 23:12:59 +0100

    share session accros subdomains

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-IntranetSXB::Application.config.session_store :cookie_store, key: '_IntranetSXB_session'
+IntranetSXB::Application.config.session_store :cookie_store, key: '_IntranetSXB_session', domain: :all
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
