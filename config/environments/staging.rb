822bf5921d021d91bd0f81d6ca73f1ed05cfb9c5
Soulou <leo.unbek@gmail.com>
2013-02-06 16:59:01 +0100

    Add albums.js to precompiled assets

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -49,7 +49,7 @@ IntranetSXB::Application.configure do
   config.action_controller.asset_host = "//#{ENV['FOG_DIRECTORY']}.s3.amazonaws.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( admin.css admin.js developers.css developers.js )
+  config.assets.precompile += %w( admin.css admin.js developers.css developers.js albums.js)
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
