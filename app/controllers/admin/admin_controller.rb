d380ff187db1293e6a1021e73c1bfe8d465b2f81
Soulou <leo@unbekandt.eu>
2013-02-19 17:32:28 +0100

    User must be admin to access the admin subdomain

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,5 +1,11 @@
 class Admin::AdminController < ApplicationController
   prepend_before_filter :authenticate_user!
+  before_filter :authenticate_admin
   layout 'admin'
   respond_to :html, :json
+
+  protected
+  def authenticate_admin
+    raise 'Unauthorized' unless current_user.is_admin?
+  end
 end
