ae3a162d1492cea2a550f742874b08a43fcd6607
Soulou <leo@unbekandt.eu>
2013-02-19 19:48:59 +0100

    Fix paperclip subdomain URL for S3

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -81,6 +81,7 @@ IntranetSXB::Application.configure do
 
   config.paperclip_defaults = {
     :storage => :s3,
+    :url => ":s3_domain_url",
     :s3_credentials => {
       :bucket => ENV['FOG_DIRECTORY'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
