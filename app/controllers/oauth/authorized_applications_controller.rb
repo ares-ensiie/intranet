1600368ae157044164e523283ab107580d87c139
Soulou <leo@unbekandt.eu>
2014-03-24 21:42:29 +0000

    Override authorized application deletion to replace the slug by the actual ID

--- app/controllers/oauth/authorized_applications_controller.rb
+++ app/controllers/oauth/authorized_applications_controller.rb
@@ -1,3 +1,10 @@
 class Oauth::AuthorizedApplicationsController < Doorkeeper::AuthorizedApplicationsController
   layout 'application'
+
+  def destroy
+    # By default params[:id] is the slug, we need the real id
+    app = Application.find params[:id]
+    Doorkeeper::AccessToken.revoke_all_for app.id, current_resource_owner
+    redirect_to oauth_authorized_applications_url, :notice => I18n.t(:notice, :scope => [:doorkeeper, :flash, :authorized_applications, :destroy])
+  end
 end
