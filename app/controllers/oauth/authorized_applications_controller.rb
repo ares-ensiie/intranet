8d369d4ed1c285a173ec6f5d48c7bcc19bb8a57a
Soulou <leo@unbekandt.eu>
2014-09-15 18:42:30 +0200

    Fix crash token revokation

--- app/controllers/oauth/authorized_applications_controller.rb
+++ app/controllers/oauth/authorized_applications_controller.rb
@@ -3,7 +3,7 @@ class Oauth::AuthorizedApplicationsController < Doorkeeper::AuthorizedApplicatio
 
   def destroy
     # By default params[:id] is the slug, we need the real id
-    app = Application.find params[:id]
+    app = Oauth::Application.find params[:id]
     Doorkeeper::AccessToken.revoke_all_for app.id, current_resource_owner
     redirect_to oauth_authorized_applications_url, :notice => I18n.t(:notice, :scope => [:doorkeeper, :flash, :authorized_applications, :destroy])
   end
