e0aa9f88dc03fcd924d61fdbcdcd54c387576679
Paul Chobert <paul@chobert.fr>
2014-09-16 11:29:41 +0200

    force response format to json

--- app/controllers/api/v1/api_v1_controller.rb
+++ app/controllers/api/v1/api_v1_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::ApiV1Controller < ActionController::Base
+  before_action :set_response_format
+
   def authenticate_application!
     @application = Doorkeeper::Application.authenticate(params[:client_id], params[:client_secret])
     raise 'Unauthorized' unless @application.present?
@@ -10,5 +12,10 @@ class Api::V1::ApiV1Controller < ActionController::Base
 
   def current_ability
     @_current_ability ||= Api::V1::Ability.new(current_application)
+  end
+
+  protected
+  def set_response_format
+    request.format = :json
   end
 end
